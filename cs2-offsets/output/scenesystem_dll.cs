943ce79cd0bbc3a9ee8daffeb28f670a7f42ba73
ZARS0W0 <tayef323@gmail.com>
2026-07-01 20:16:44 +0200

    x
    x

--- cs2-offsets/output/scenesystem_dll.cs
+++ cs2-offsets/output/scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-30 12:47:55.322834100 UTC
+// 2026-07-01 18:16:15.155884100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
